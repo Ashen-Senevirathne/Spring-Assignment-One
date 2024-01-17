f56890ba4b8412918d5fbb81cd95ebb09e31f814
Ashen Senevirathne <ashensachin@gmail.com>
2024-01-17 13:06:04 +0530

    Feature completed

--- src/main/java/com/fidenz/assignmentone/service/UserService.java
+++ src/main/java/com/fidenz/assignmentone/service/UserService.java
@@ -2,10 +2,17 @@ package com.fidenz.assignmentone.service;
 
 import com.fidenz.assignmentone.repository.IUserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService implements IUserService {
+
+    @Value("${app.username}")
+    private String username;
+
+    @Value("${app.password}")
+    private String password;
 
     private final IUserRepository userRepository;
 
@@ -16,6 +23,6 @@ public class UserService implements IUserService {
 
     @Override
     public String getData() {
-        return userRepository.getData();
+        return userRepository.getData() + "\n" + "Username: " + username + "\n" + "Password: " + password;
     }
 }
