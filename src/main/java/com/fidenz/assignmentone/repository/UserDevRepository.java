b4faef01f446f6805c22052940884124b1fe9b6b
Ashen Senevirathne <ashensachin@gmail.com>
2024-01-17 15:43:39 +0530

    Load env variables function added

--- src/main/java/com/fidenz/assignmentone/repository/UserDevRepository.java
+++ src/main/java/com/fidenz/assignmentone/repository/UserDevRepository.java
@@ -4,7 +4,7 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Repository;
 
 @Repository
-@Profile("dev")
+@Profile({"dev", "default"})
 public class UserDevRepository implements IUserRepository{
     @Override
     public String getData() {
